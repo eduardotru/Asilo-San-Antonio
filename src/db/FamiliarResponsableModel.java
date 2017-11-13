f7e9bdf0f1874da7dbe4b995bdff7d27eee3ab51
Eduardo Trujillo <ed.trujillo.r@outlook.com>
2017-11-13 11:45:03 -0600

    FamiliarResponsable objeto y modelo agregado

--- src/db/FamiliarResponsableModel.java
+++ src/db/FamiliarResponsableModel.java
@@ -5,6 +5,8 @@ import objetos.FamiliarResponsable;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class FamiliarResponsableModel extends InterfazDB
 {
@@ -33,26 +35,41 @@ public class FamiliarResponsableModel extends InterfazDB
         return familiarId;
     }
 
-  /*  public FamiliarResponsable selectFamilar(int id) throws SQLException {
-        FamiliarResponsable familiar = null;
+    public void addFamiliarResponsable(FamiliarResponsable familiarResponsable) throws Exception
+    {
+        try {
+            int id = addFamiliarResponsable(familiarResponsable.getNombre(), familiarResponsable.getRelacion(),
+                    familiarResponsable.getTelefono(), familiarResponsable.getIdPaciente());
+            familiarResponsable.setId(id);
+        }
+        catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public int addFamiliarResponsable(String nombre, String relacion, String telefono,
+                                      int idPaciente) throws Exception
+    {
+        int id = -1;
         try {
             InterfazDB.crearConexion();
         } catch (Exception e){
-            System.err.println("Error al obtener un evento: "
+            System.err.println("Error al agregar un familiarResponsable: "
                     + e.getClass().getName() + ": " + e.getMessage());
         }
         try {
             PreparedStatement prepStatement = c.prepareStatement(
-                    "SELECT * FROM Asilo.familiarresponsable AS f WHERE f.id = ?");
-            ResultSet result = prepStatement.executeQuery();
-            if (result.next()) {
-                int idFamiliar = result.getInt("id");
-                String sNombre = result.getString("nombre");
-                String sRelacion = result.getString("relacion");
-                String sTelefono = result.getString("telefono");
-                int idPaciente = result.getInt("idPaciente");
-                familiar = new FamiliarResponsable(idFamiliar, sNombre, sRelacion, sTelefono,
-                        idPaciente);
+                    "INSERT INTO Asilo.FamiliarResponsable VALUES (default, ?, ?, ?, ?)");
+            prepStatement.setString(1, nombre);
+            prepStatement.setString(2, relacion);
+            prepStatement.setString(3, telefono);
+            prepStatement.setInt(4, idPaciente);
+            prepStatement.executeUpdate();
+
+            ResultSet result = prepStatement.getGeneratedKeys();
+
+            if(result.next()) {
+                id = result.getInt(1);
             }
         }
         catch (Exception e) {
@@ -61,6 +78,160 @@ public class FamiliarResponsableModel extends InterfazDB
         finally {
             cerrarConexion();
         }
-        return familiar;
-    }*/
+        return id;
+    }
+
+    public FamiliarResponsable selectFamiliarResponsable(int id) throws Exception
+    {
+        FamiliarResponsable familiarResponsable;
+        try {
+            InterfazDB.crearConexion();
+        } catch (Exception e){
+            System.err.println("Error al obtener los familiarResponsables: "
+                    + e.getClass().getName() + ": " + e.getMessage());
+        }
+        try {
+            PreparedStatement  prepeparedStatement = c.prepareStatement(
+                    "SELECT * FROM Asilo.FamiliarResponsable WHERE id = ?");
+            prepeparedStatement.setInt(1, id);
+            ResultSet result = prepeparedStatement.executeQuery();
+            FamiliarResponsable[] familiarResponsables = crearListaFamiliarResponsables(result);
+            if(familiarResponsables.length == 1) {
+                familiarResponsable = familiarResponsables[0];
+            }
+            else {
+                familiarResponsable = null;
+            }
+        }
+        catch (Exception e) {
+            throw e;
+        }
+        finally {
+            cerrarConexion();
+        }
+        return familiarResponsable;
+    }
+
+    public int selectIdFamiliarResponsable(String nombre) throws Exception
+    {
+        int id = -1;
+        try {
+            InterfazDB.crearConexion();
+        } catch (Exception e){
+            System.err.println("Error al obtener los familiarResponsables: "
+                    + e.getClass().getName() + ": " + e.getMessage());
+        }
+        try {
+            PreparedStatement preparedStatement = c.prepareStatement(
+                    "SELECT id FROM Asilo.FamiliarResponsable WHERE nombre = ?");
+            preparedStatement.setString(1, nombre);
+
+            preparedStatement.executeQuery();
+
+            ResultSet resultSet = preparedStatement.getResultSet();
+
+            if(resultSet.next()) {
+                id = resultSet.getInt(1);
+            }
+        }
+        catch (Exception e) {
+            throw e;
+        }
+        finally {
+            cerrarConexion();
+        }
+        return id;
+    }
+
+    public FamiliarResponsable[] selectFamiliarResponsablesPorPaciente(int idPaciente) throws Exception
+    {
+        FamiliarResponsable[] familiarResponsables;
+        try {
+            InterfazDB.crearConexion();
+        } catch (Exception e){
+            System.err.println("Error al obtener los familiarResponsables: "
+                    + e.getClass().getName() + ": " + e.getMessage());
+        }
+        try {
+            PreparedStatement prepStatement = c.prepareStatement(
+                    "SELECT * FROM Asilo.FamiliarResponsable WHERE idPaciente = ?");
+            prepStatement.setInt(1, idPaciente);
+            prepStatement.executeQuery();
+            ResultSet result = prepStatement.getResultSet();
+            familiarResponsables = crearListaFamiliarResponsables(result);
+        }
+        catch (Exception e) {
+            throw e;
+        }
+        finally {
+            cerrarConexion();
+        }
+        return familiarResponsables;
+    }
+
+    public FamiliarResponsable[] selectFamiliarResponsables() throws Exception
+    {
+        FamiliarResponsable[] familiarResponsables;
+        try {
+            InterfazDB.crearConexion();
+        } catch (Exception e){
+            System.err.println("Error al obtener los familiarResponsables: "
+                    + e.getClass().getName() + ": " + e.getMessage());
+        }
+        try {
+            Statement statement = c.createStatement();
+            ResultSet result = statement.executeQuery(
+                    "SELECT * FROM Asilo.FamiliarResponsable");
+            familiarResponsables = crearListaFamiliarResponsables(result);
+        }
+        catch (Exception e) {
+            throw e;
+        }
+        finally {
+            cerrarConexion();
+        }
+        return familiarResponsables;
+    }
+
+    private FamiliarResponsable[] crearListaFamiliarResponsables(ResultSet result) throws SQLException
+    {
+        ArrayList<FamiliarResponsable> listaFamiliarResponsables = new ArrayList<FamiliarResponsable>();
+        while(result.next())
+        {
+            int id = result.getInt("id");
+            String nombre = result.getString("nombre");
+            String relacion = result.getString("relacion");
+            String telefono = result.getString("telefono");
+            int idPaciente = result.getInt("idPaciente");
+            listaFamiliarResponsables.add(new FamiliarResponsable(id, nombre, relacion, telefono, idPaciente));
+        }
+        FamiliarResponsable[] arrFamiliarResponsables = new FamiliarResponsable[listaFamiliarResponsables.size()];
+        listaFamiliarResponsables.toArray(arrFamiliarResponsables);
+        return arrFamiliarResponsables;
+    }
+
+    public void updateFamiliarResponsable(FamiliarResponsable familiarResponsable) throws Exception
+    {
+        try {
+            InterfazDB.crearConexion();
+        } catch (Exception e){
+            System.err.println("Error al modificar un familiarResponsable: "
+                    + e.getClass().getName() + ": " + e.getMessage());
+        }
+        try {
+            PreparedStatement prepStatement = c.prepareStatement(
+                    "UPDATE Asilo.FamiliarResponsable " +
+                            "SET nombre = ? WHERE id = ?");
+            prepStatement.setString(1, familiarResponsable.getNombre());
+            prepStatement.setInt(2, familiarResponsable.getId());
+
+            prepStatement.executeUpdate();
+        }
+        catch (Exception e) {
+            throw e;
+        }
+        finally {
+            cerrarConexion();
+        }
+    }
 }
--- src/objetos/FamiliarResponsable.java
+++ src/objetos/FamiliarResponsable.java
@@ -1,11 +1,14 @@
 package objetos;
 
+import db.PacienteModel;
+
 public class FamiliarResponsable {
     private int id = -1;
     private String sNombre;
     private String sRelacion;
     private String sTelefono;
     private int idPaciente;
+    private Paciente paciente;
 
     public FamiliarResponsable(int id, String sNombre, String sRelacion,
                                String sTelefono, int idPaciente){
@@ -16,7 +19,54 @@ public class FamiliarResponsable {
         this.idPaciente = idPaciente;
     }
 
-    public String getName() {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        if(id == -1) {
+            this.id = id;
+        }
+    }
+
+    public String getNombre() {
         return this.sNombre;
+    }
+
+    public void setNombre(String sNombre) {
+        this.sNombre = sNombre;
+    }
+
+    public String getRelacion() {
+        return sRelacion;
+    }
+
+    public void setRelacion(String sRelacion) {
+        this.sRelacion = sRelacion;
+    }
+
+    public String getTelefono() {
+        return sTelefono;
+    }
+
+    public void setTelefono(String sTelefono) {
+        this.sTelefono = sTelefono;
+    }
+
+    public int getIdPaciente() {
+        return idPaciente;
+    }
+
+    public Paciente getPaciente() {
+        if(paciente == null && idPaciente != -1) {
+            try {
+                PacienteModel pacienteModel = new PacienteModel();
+                paciente = pacienteModel.selectPaciente(idPaciente);
+            }
+            catch(Exception e) {
+                System.out.println("Error al obtener un paciente");
+            }
+        }
+        return paciente;
     }
 }
